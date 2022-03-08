07996015166a4e257f4a996a08459ac1b498c068
krish <krishgoti2002@gmail.com>
2022-03-08 22:13:00 +0530

    admin classroom deletion and graph label and admin user deletion

--- classroom/backend/routes/adminRoute.js
+++ classroom/backend/routes/adminRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('./../models/User')
 const Classroom = require('./../models/Classroom')
 const Assignment = require('./../models/Assignment')
+const Submission = require('../models/Submission')
 const router = express.Router()
 
 router.get('/fetchAllUser', async (req, res) => {
@@ -17,9 +18,9 @@ router.get('/fetchAllUser', async (req, res) => {
 
 router.get('/fetchUser', async (req, res) => {
     try {
-        let User = await User.findOne({ _id: req.header('id') }).populate('classrooms');
-        // res.send({ User })
-        res.send({success: req.header('id')})
+        const user = await User.findOne({_id: req.header('id') }).populate('classrooms');
+        res.send({ user })
+        // res.send({success: req.header('id')})
     }
     catch (err) {
         // res.status(500).send("Internal server error")
@@ -27,4 +28,48 @@ router.get('/fetchUser', async (req, res) => {
     }
 })
 
+router.delete('/deleteClassroom',async(req,res) => {
+    try {
+        const classroom = await Classroom.findById({_id:req.body.id});
+        console.log(classroom);
+        /** Deleting classroom from user model */
+        const deletedClassroomFromUser = await User.find({_id:{ $in: classroom.members }}).updateMany({$pull:{classrooms: req.body.id}});
+        // const ans = await User.find({_id:{ $in: classroom.members }}).updateMany({ $push : {classrooms:req.body.id}})
+        console.log(deletedClassroomFromUser);
+        /** Searching assignments */
+        const assignment = await Assignment.find({_id:{ $in: classroom.assignments}});
+        /** Searching submissions and deleting */
+        let allSubmissions = [];
+        assignment.map(a => {
+            allSubmissions = [...allSubmissions,...a.submissions]
+        })
+        const deletedSubmission = await Submission.deleteMany({_id:{$in : allSubmissions}});
+        console.log(deletedSubmission);
+        /**------------------------------------- */
+        /** Deleting assignement */
+        const deletedAssignment = await Assignment.deleteMany({_id : { $in : classroom.assignments}})
+        console.log(deletedAssignment);
+        /**--------------------- */
+        /** Deleting classroom */
+        const deletedClassroom = await Classroom.deleteOne({_id:req.body.id});
+        console.log(deletedClassroom);
+        /**-------------------- */
+        res.send({res: "success"});
+    }
+    catch (err) {
+        // res.status(500).send("Internal server error")
+        console.log(err.message)        
+    }
+});
+
+router.delete('/deleteUser',async(req,res) => {
+    try {
+        const user = await User.deleteOne({_id:req.body.id});
+        res.send({res: "success"});
+    }
+    catch (err) {
+        // res.status(500).send("Internal server error")
+        console.log(err.message)        
+    }
+});
 module.exports = router
--- classroom/src/components/admin/TableRow.js
+++ classroom/src/components/admin/TableRow.js
@@ -2,13 +2,36 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function TableRow({ user, index }) {
+
+    const deleteHandler = async(id) => {
+        try {
+            let response = await fetch('http://localhost:4099/api/admin/deleteUser', 
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id : id,
+                }),
+            });
+            console.log(response.json().res);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <tr>
             <th scope="row">{}</th>
             <td>{user?.UID}</td>
             <td>{user?.name}</td>
             <td>{user?.email}</td>
-            <td><Link to={`/admin/viewuser/${user._id}`}><i style={{fontSize:"24px"}} className="fa">&#xf06e;</i></Link></td>
+            <td>
+                <Link to={`/admin/viewuser/${user._id}`}><i style={{fontSize:"24px"}} className="fa">&#xf06e;</i></Link>
+                <i className='fa fa-trash' onClick={()=>deleteHandler(user._id)} />
+            </td>
         </tr>
     )
 }
