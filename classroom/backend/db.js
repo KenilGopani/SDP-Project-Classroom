ee847c191bee4268aabe61d06ce5fcb8af201068
KenilGopani <kenilgopani600@gmail.com>
2022-03-09 18:55:17 +0530

    mongoDB Atlas connected to backend

--- classroom/backend/db.js
+++ classroom/backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const credential = require('./emailPassword')
-// const URI = "mongodb://localhost:27017/Classroom?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
+// For local mongoDB storage 
+// const URI = "mongodb://localhost:27017/Classroom?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 // const connectToMongo = () => {
 
 //     mongoose.connect(URI,()=>{
@@ -9,18 +10,18 @@ const credential = require('./emailPassword')
 //     })
 // }
 
-const AtlasURI = `mongodb+srv://klassroom0246:${credential.pass}@cluster0.sqqbw.mongodb.net/KlassroomDB?retryWrites=true&w=majority`;
-
+// for mongoDB Atlas.
+const AtlasURI = `mongodb+srv://klassroom0246:${credential.mongoDBPass}@cluster0.sqqbw.mongodb.net/KlassroomDB?retryWrites=true&w=majority`;
 const connectToMongo = () => {
 
     mongoose.connect(AtlasURI,{
-        useNewUrlParser : true,
-        useCreateIndex: true,
-        useUnifiedTopology : true,
-        useFindAndModify: false
+        // useNewUrlParser : true,
+        // useCreateIndex: true,
+        // useUnifiedTopology : true,
+        // useFindAndModify: false
     }).then(()=>{
         console.log("Connected to Mongo!");
-    }).catch((err)=> console.log('NOt connected'))
+    }).catch((err)=> console.log(err))
 }
 
 module.exports = connectToMongo;
